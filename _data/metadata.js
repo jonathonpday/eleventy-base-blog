5c6057fa55e452fe0d8dce1b5abd12b3e46b1bf2
jonathonpday <jonathonpday@me.com>
2024-01-30 19:45:35 +0000

    Initial Changes made in lesson

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://jpdportfolio.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "This is a portfolio of my work .",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Jonathon Day",
+		email: "jonathonpday@me.com",
+		url: "https://jpdportfolio.com/about-me/"
 	}
 }
